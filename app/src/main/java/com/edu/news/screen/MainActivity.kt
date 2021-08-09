164adef7a9743a5da75a5a64901d4bbcdf1ecabc
okuloj <viettrung2005.99@gmail.com>
2021-08-09 11:18:01 +0700

    create UI menu

---
+++ app/src/main/java/com/edu/news/screen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.edu.news.screen
+
+import com.edu.news.utils.base.BaseActivity
+import com.sun.news.R
+
+class MainActivity : BaseActivity() {
+
+    override fun getLayoutResourceId() = R.layout.activity_main
+
+    override fun initView() {
+    }
+}
